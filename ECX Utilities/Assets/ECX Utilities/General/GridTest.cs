55d9668ee024a223d97638a27795f341ae5abe2c
Brian Leip <brianleip@gmail.com>
2022-01-17 22:34:31 -0800

    Added Gridd system with some helper text mesh pro creation utils

--- ECX Utilities/Assets/ECX Utilities/General/GridTest.cs
+++ ECX Utilities/Assets/ECX Utilities/General/GridTest.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 using EcxUtilities;
 
 public class GridTest : MonoBehaviour {
+    Gridd grid;
+    int width = 10;
+    int height = 10;
+    float cellsize = 1f;
+
     private void Start() {
-        Gridd grid = new Gridd(10, 10, 1f);
+        grid = new Gridd(width, height, cellsize);
+        grid.SetValue(2, 1, 13);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            grid.SetValue(Random.Range(0, width), Random.Range(0, height), Random.Range(1, 999));
+        }
     }
 }
--- ECX Utilities/Assets/ECX Utilities/General/Gridd.cs
+++ ECX Utilities/Assets/ECX Utilities/General/Gridd.cs
@@ -1,33 +1,41 @@
 ﻿/*
 ECX UTILITY SCRIPTS
 Gridd (uses double d's to avoid name conflict with UnityEngine.grid)
-Last updated: Jan 13, 2022
+Last updated: Jan 17, 2022
 */
 
 using UnityEngine;
+using TMPro;
 
 namespace EcxUtilities {
     public class Gridd {    // (uses double d's to avoid name conflict with UnityEngine.grid)
+        private static Transform textContainerTfm;
         private int width;
         private int height;
         private float cellSize;
         private int[,] gridArray;
+        private TextMeshPro[,] gridTextArray;   // to display values stored in gridArray
 
         // Constructor
-        public Gridd(int width, int height, float cellSize) {
+        public Gridd(int width, int height, float cellSize = 1f, bool displayValues = true, bool displayGridLines = true) {
             this.width = width;
             this.height = height;
             this.cellSize = cellSize;
             gridArray = new int[width, height];
-
+            // Debug text
+            gridTextArray = new TextMeshPro[width, height];
+            if (!textContainerTfm) {
+                GameObject textContainerGO = new GameObject("Text Container");
+                textContainerTfm = textContainerGO.transform;
+            }
             for (int x=0; x<gridArray.GetLength(0); x++) {
                 for (int y=0; y<gridArray.GetLength(1); y++) {
-                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y+1), Color.white, 100f);
-                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y), Color.white, 100f);
+                    gridTextArray[x,y] = Tmp.CreateWorldTmp(gridArray[x,y].ToString(), textContainerTfm, GetWorldPosition(x,y) + new Vector3(cellSize/2, cellSize/2), (int)Mathf.Round(5*cellSize), Color.white, TextAlignmentOptions.Center, 5000);
+                    gridTextArray[x,y].gameObject.SetActive(displayValues);
                 }
             }
-            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
-            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+            if (displayGridLines == true)
+                DisplayGridLines();
         }
 
         private (int,int) GetGridPosition(Vector3 worldPosition) {
@@ -42,7 +50,44 @@ namespace EcxUtilities {
 
         public void SetValue(int x, int y, int value) {
             gridArray[x, y] = value;
+            gridTextArray[x,y].text = value.ToString();
         }
-        
+
+        public void SetValue(Vector3 worldPosition, int value) {
+            int x, y;
+            (x, y) = GetGridPosition(worldPosition);
+            SetValue(x, y, value);
+            Debug.Log("Set (" + x + ", " + y + ") to " + value);
+        }
+
+        public void DisplayValues() {
+            Vector3 centeringOffset = new Vector3(cellSize/2, cellSize/2);
+            for (int x=0; x<gridArray.GetLength(0); x++) {
+                for (int y=0; y<gridArray.GetLength(1); y++) {
+                    gridTextArray[x,y].transform.position = GetWorldPosition(x, y) + centeringOffset;
+                    gridTextArray[x,y].text = gridArray[x,y].ToString();
+                    gridTextArray[x,y].gameObject.SetActive(true);
+                }
+            }
+        }
+
+        public void HideValues() {
+            for (int x=0; x<gridArray.GetLength(0); x++) {
+                for (int y=0; y<gridArray.GetLength(1); y++) {
+                    gridTextArray[x,y].gameObject.SetActive(false);
+                }
+            }
+        }
+
+        public void DisplayGridLines() {
+            for (int x=0; x<gridArray.GetLength(0); x++) {
+                for (int y=0; y<gridArray.GetLength(1); y++) {
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y+1), Color.white, 100f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y), Color.white, 100f);
+                }
+            }
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+        }
     }
 }
