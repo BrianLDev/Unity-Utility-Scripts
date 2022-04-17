889d6fb3d2348a264da2a7e9ed84cc80171d70bf
Brian Leip <brianleip@gmail.com>
2022-04-16 23:06:03 -0700

    Object Pooling scripts and demos

---
+++ ECX Utilities/Assets/ECX Utilities/Object Pooling/Demos/PoolingGameObjects/Bullet.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using EcxUtilities;
+
+public class Bullet : MonoBehaviour {
+	private float speed, accel;
+
+	void OnEnable() {
+		speed = 0f;
+    accel = 0.2f;
+
+	}
+
+  private void FixedUpdate() {
+    speed += accel;
+    transform.position += Vector3.up * speed * Time.fixedDeltaTime;
+
+		if(transform.position.y > 10)
+			Release();
+  }
+
+	void Release() {
+		PoolManager.ReleaseObject(this.gameObject);
+	}
+}
---
+++ ECX Utilities/Assets/ECX Utilities/Object Pooling/Demos/PoolingGameObjects/Gun.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using EcxUtilities;
+
+public class Gun : MonoBehaviour
+{
+	[SerializeField] private GameObject bulletPrefab;
+  private Vector3 pos;
+
+	//Optional: Warm the pool and preallocate memory
+	void Start() {
+    bulletPrefab.SetActive(false);
+		PoolManager.CreatePool(bulletPrefab, 50);
+	}
+
+	void Update() {
+		if(Input.GetButton("Fire1")) {
+      pos.x = Input.mousePosition.x;
+      pos.y = Input.mousePosition.y;
+      pos.z = 10f;
+			FireBullet(Camera.main.ScreenToWorldPoint(pos), Quaternion.identity);	
+		}
+	}
+
+	void FireBullet(Vector3 position, Quaternion rotation) {
+		PoolManager.SpawnObject(bulletPrefab, position, rotation).GetComponent<Bullet>();
+	}
+
+}
